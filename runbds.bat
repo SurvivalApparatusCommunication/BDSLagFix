97613134a0072c86e455aa059b467051bb8edcf8
OEOTYAN <1124718975@qq.com>
2023-03-18 23:42:48 +0800

    update

---
+++ runbds.bat
@@ -0,0 +1,15 @@
+set TARGET_PATH=C:\Unzip\SAC\bedrock-server-1.19.61.01
+
+if exist %TARGET_PATH%\bedrock_server_mod.exe goto process
+
+@echo Failed to find BDS path, will not run BDS
+
+goto end
+
+:process
+xcopy Release\%1.dll %TARGET_PATH%\plugins /Y
+xcopy Release\%1.pdb %TARGET_PATH%\plugins /Y
+
+wt %TARGET_PATH%\bedrock_server_mod.exe
+
+:end
